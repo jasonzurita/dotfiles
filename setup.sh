f71bfccbc4cf65fa250b4c6f6fe042a78ee2b4a5
Jason Zurita <jasonzurita@me.com>
2018-02-09 09:37:27 -0500

    Add Vundle install to setup.sh

--- setup.sh
+++ setup.sh
@@ -19,4 +19,11 @@ ls "$DOTFILES" | grep -v '\.sh' | grep -v '\.md' | while read DOTFILE; do
 	fi
 done
 
+echo -n "Installing Vundle..."
+if [ -a "$HOME/.vim/bundle/Vundle.vim" ]; then
+	echo " exists."
+else
+	git clone "https://github.com/VundleVim/Vundle.vim.git" "$HOME/.vim/bundle/Vundle.vim"
+fi
+
 echo "Done."
